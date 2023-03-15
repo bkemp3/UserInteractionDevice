081e2c95b66e1903a6898a07e53dc495e2444f0c
bkemp3 <bkemp3@jhu.edu>
2023-03-14 20:28:30 -0400

    first commit

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,28 @@
+using System;
+
+[Serializable]
+
+
+public struct PlayerData
+{
+    public float qw;
+    public float qx;
+    public float qy;
+    public float qz;
+    public float button;
+    public float shaking;
+    public float joyx;
+    public float joyy;
+    public string id;
+    public double timestamp;
+}
+/*
+public struct PlayerData
+{
+    public string id;
+    public float xPos;
+    public float yPos;
+    public float zPos;
+    public double timestamp;
+}
+*/
